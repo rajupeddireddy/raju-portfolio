ec4267467652cb52b59bb1a45a714f0fcc292faa
rajupeddireddy <ptele0983@gmail.com>
2024-04-30 12:51:49 +0530

    removed all unnessary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Home from './components/Home/Home';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Home/>
-      </BrowserRouter>
+      Hiii
       
     </div>
   );
